c708093aa8593ef2715e9281735bfa10fb49c6c6
Joonwon Han <hjw.github.ac@gmail.com>
2024-02-09 06:40:42 +0900

    Initial Commit

---
+++ TestGithubSetup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestGithubSetup
+//
+//  Created by 준원한 on 2024/02/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestGithubSetup/TestGithubSetupApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGithubSetupApp.swift
+//  TestGithubSetup
+//
+//  Created by 준원한 on 2024/02/09.
+//
+
+import SwiftUI
+
+@main
+struct TestGithubSetupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
